78fc190b0169c586849f89ab38eddf5082451ae6
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-05-31 19:59:12 +0100

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name="pyramid_services_viewmapper",
+    version="0.0",
+    description="View mapper for use with pyramid_services.",
+    long_description="",
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author="",
+    author_email="",
+    url="",
+    keywords="web wsgi bfg pylons pyramid",
+    packages=["pyramid_services_viewmapper"],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="",
+    install_requires=[],
+    entry_points="",
+)
