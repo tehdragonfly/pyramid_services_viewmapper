5bd009d9f75c1938451f5168dbcefde9d6ce95b3
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-10-17 17:58:51 +0100

    Populate setup.py.

--- setup.py
+++ setup.py
@@ -1,25 +1,107 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = "pyramid_services_viewmapper"
+DESCRIPTION = "View mapper for use with pyramid_services."
+URL = "https://github.com/tehdragonfly/pyramid_services_viewmapper"
+EMAIL = "mysticdragonfly@hotmail.co.uk"
+AUTHOR = "Olly Parton"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "1.0.0"
+
+INSTALL_REQUIRES = ["pyramid_services"]
+
+TESTS_REQUIRE = []
+
+EXTRAS = {}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
 
 
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag {0}".format(VERSION))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name="pyramid_services_viewmapper",
-    version="0.0",
-    description="View mapper for use with pyramid_services.",
-    long_description="",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=["pyramid_services_viewmapper"],
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require=EXTRAS,
+    include_package_data=True,
     classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
-        "Framework :: Pyramid",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Framework :: Pyramid",
     ],
-    author="",
-    author_email="",
-    url="",
-    keywords="web wsgi bfg pylons pyramid",
-    packages=["pyramid_services_viewmapper"],
-    include_package_data=True,
-    zip_safe=False,
-    test_suite="",
-    install_requires=["pyramid_services"],
-    entry_points="",
+    # $ setup.py publish support.
+    cmdclass={
+        "upload": UploadCommand,
+    },
 )
+
