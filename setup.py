7132f0f9e9356c944c8547c72545b71ff0382683
Olly Parton <mysticdragonfly@hotmail.co.uk>
2019-10-30 18:21:29 +0000

    Add 3.8 tag.

--- setup.py
+++ setup.py
@@ -93,6 +93,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: WWW/HTTP",
